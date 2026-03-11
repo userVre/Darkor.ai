f7db4842d7acec9fe28610bedcdc61933e9defc8
userVre <123872204+userVre@users.noreply.github.com>
2026-03-11 02:18:59 +0000

    Inline metro exclusionList to avoid export error

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,25 @@
+const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-const exclusionList = require("metro-config/src/defaults/exclusionList");
+
+const defaultExclusions = [/\/__tests__\/.*/];
+
+function escapeRegExp(pattern) {
+  if (pattern instanceof RegExp) {
+    return pattern.source.replace(/\/|\\\//g, "\\" + path.sep);
+  }
+  if (typeof pattern === "string") {
+    const escaped = pattern.replace(/[\-\[\]\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+    return escaped.replaceAll("/", "\\" + path.sep);
+  }
+  throw new Error(`Expected exclusionList to be called with RegExp or string, got: ${typeof pattern}`);
+}
+
+function exclusionList(additionalExclusions) {
+  return new RegExp(
+    "(" + (additionalExclusions || []).concat(defaultExclusions).map(escapeRegExp).join("|") + ")$",
+  );
+}
 
 const config = getDefaultConfig(__dirname);
 config.resolver.assetExts.push("mp4");
