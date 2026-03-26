f47a6c637d648b00ed79f0e5ba85466aa1d40d33
userVre <123872204+userVre@users.noreply.github.com>
2026-03-26 21:58:08 +0100

    i

--- app.config.ts
+++ app.config.ts
@@ -7,12 +7,12 @@ module.exports = () => ({
   extra: {
     ...expoConfig.extra,
     publicEnv: {
-      clerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
-      convexUrl: process.env.EXPO_PUBLIC_CONVEX_URL,
-      revenueCatIosKey: process.env.EXPO_PUBLIC_REVENUECAT_IOS_API_KEY,
-      revenueCatAndroidKey: process.env.EXPO_PUBLIC_REVENUECAT_ANDROID_API_KEY,
-      revenueCatKey: process.env.EXPO_PUBLIC_REVENUECAT_API_KEY,
-      apiBaseUrl: process.env.EXPO_PUBLIC_API_BASE_URL,
+      EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
+      EXPO_PUBLIC_CONVEX_URL: process.env.EXPO_PUBLIC_CONVEX_URL,
+      EXPO_PUBLIC_REVENUECAT_IOS_API_KEY: process.env.EXPO_PUBLIC_REVENUECAT_IOS_API_KEY,
+      EXPO_PUBLIC_REVENUECAT_ANDROID_API_KEY: process.env.EXPO_PUBLIC_REVENUECAT_ANDROID_API_KEY,
+      EXPO_PUBLIC_REVENUECAT_API_KEY: process.env.EXPO_PUBLIC_REVENUECAT_API_KEY,
+      EXPO_PUBLIC_API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL,
     },
   },
 });
--- expo-env.d.ts
+++ expo-env.d.ts
@@ -1,7 +1,3 @@
 /// <reference types="expo/types" />
 
 // NOTE: This file should not be edited and should be in your git ignore
-
-declare module "expo/virtual/env" {
-  export const env: Record<string, string | undefined>;
-}
